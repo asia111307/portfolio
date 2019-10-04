e48c0c55932cb1abcad154fe168c6440de8cba70
asia111307 <aspal13@interia.pl>
2019-10-04 16:28:24 +0200

    highlited menu added

--- src/app/app-top-nav/app-top-nav.component.ts
+++ src/app/app-top-nav/app-top-nav.component.ts
@@ -36,9 +36,9 @@ export class AppTopNavComponent implements OnInit {
           this.classList.toggle('open');
           const menu_items = document.getElementsByClassName('nav-hidden');
           const menu = this;
-          for (let i=0; i< menu_items.length; i++) {
+          for (let i = 0; i < menu_items.length; i++) {
               if (menu.classList.contains('open')) {
-                  (<HTMLElement>menu_items[i]).style.display = 'flex';
+                  (<HTMLElement> menu_items[i]).style.display = 'flex';
               } else {
                   (<HTMLElement>menu_items[i]).style.display = 'none';
               }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,33 @@ export class AppComponent implements OnInit {
        this.changeLangService.currentLanguagePack$.subscribe((newPack: any) => { this.currentPack = newPack; });
   }
   setDefaultLangEmitters() {
-        const elementENG = document.getElementsByClassName('lang-switch-to-eng')[0]
+        document.getElementsByClassName('lang-switch-to-eng')[0]
             .addEventListener('click', this.changeLangService.changeLangToENG);
-        const elementPL = document.getElementsByClassName('lang-switch-to-pl')[0]
+        document.getElementsByClassName('lang-switch-to-pl')[0]
             .addEventListener('click', this.changeLangService.changeLangToPL);
   };
+  setEventListenerScroll() {
+        window.addEventListener('scroll', function () {
+            const menu_items_links = document.getElementsByClassName('nav-element-a');
+            const menu_height = (<HTMLElement>document.getElementsByClassName('navigation')[0]).offsetHeight;
+            const clearMenu= function () {
+                for (let i = 0; i < menu_items_links.length; i++) {
+                    (<HTMLElement>menu_items_links[i].firstChild).style.color = 'grey';
+                }
+            };
+            clearMenu();
+            for (let i = 0; i < menu_items_links.length; i++) {
+                const target = document.getElementById(menu_items_links[i].id.split('-')[0]);
+                if (target.getBoundingClientRect().top <= (menu_height)) {
+                    clearMenu();
+                    (<HTMLElement>menu_items_links[i].firstChild).style.color = 'rgb(45, 134, 229)';}
+            }
+        });
+  }
+
   ngOnInit() {
     this.setDefaultLangEmitters();
+    this.setEventListenerScroll();
   }
 }
 
