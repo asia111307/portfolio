0fd35042f9d33c336ab4e90078db71799893143e
asia111307 <aspal13@interia.pl>
2019-10-12 20:22:08 +0200

    shadow-box added

--- src/app/app-projects/app-projects.component.ts
+++ src/app/app-projects/app-projects.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ChangeLangService} from '../change-lang.service';
 
 @Component({
@@ -6,9 +6,25 @@ import {ChangeLangService} from '../change-lang.service';
   templateUrl: './app-projects.component.html',
   styleUrls: ['./app-projects.component.css']
 })
-export class AppProjectsComponent {
+export class AppProjectsComponent implements OnInit {
   currentPack: any;
   constructor(private changeLangService: ChangeLangService) {
     this.changeLangService.currentLanguagePack$.subscribe((newPack: any) => { this.currentPack = newPack; });
+  }
+  setEventListenerScroll() {
+    window.addEventListener('scroll', function() {
+      const projects = document.getElementsByClassName('project-item');
+      const current_window_width = window.innerWidth;
+      if (current_window_width >= 1060) {
+        for (let i = 0; i < (projects.length); i++) {
+          if (projects[i].getBoundingClientRect().top < (window.innerHeight * 0.8)) {
+            (<HTMLElement> projects[i]).style.opacity = '1';
+          }
+        }
+      }
+    });
+  };
+  ngOnInit() {
+    this.setEventListenerScroll();
   }
 }
